e961a2708feedb797874df0d119b22ad106dcddc
Kalmix <87293493+kalmix@users.noreply.github.com>
2024-10-27 23:00:06 -0400

    (fix)CORS

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,13 @@ const config = {
     port: process.env.PORT || 4000,
     wsPort: process.env.WS_PORT || 8080,
     cors: {
-      origin: process.env.NODE_ENV === 'production' ? process.env.ALLOWED_ORIGIN : '*',
+      origin: process.env.NODE_ENV === 'production' 
+        ? [
+            process.env.ALLOWED_ORIGIN,
+            'https://k4sosgskow4scg800w040cc4.150.136.160.212.sslip.io',
+            'https://test.vostro.agency'
+          ]
+        : '*',
       methods: ['GET', 'POST'],
       allowedHeaders: ['Content-Type', 'Authorization'],
       credentials: true
